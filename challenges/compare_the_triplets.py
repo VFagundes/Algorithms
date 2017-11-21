3b86d77e26a0172e3e6370271aa7f32f565787f1
V <v@localhost.localdomain>
2017-11-20 23:27:54 -0200

    compare the triplets

--- challenges/compare_the_triplets.py
+++ challenges/compare_the_triplets.py
@@ -28,20 +28,45 @@ Print two space-separated integers denoting the respective comparison points ear
 
 """
 import sys
-def is_major(a,b):
+def greater(a,b):
     return a>b
 
-def equals(a,b):
-    return a==b
 
-def solve(tA,tB):
-
+def solve(a0, a1, a2, b0, b1, b2):
+    tA =(a0,a1,a2)
+    tB = (b0,b1,b2)
+    length = len(tA)
+    result =[]
+    for i in range(0,length):
+        valA,valB= tA[i],tB[i]
+        if valA!=valB:
+            result.append(1)
+    return result
 
 a0, a1, a2 = raw_input().strip().split(' ')
-#a0, a1, a2 = [int(a0), int(a1), int(a2)]
+a0, a1, a2 = [int(a0), int(a1), int(a2)]
 b0, b1, b2 = raw_input().strip().split(' ')
-#b0, b1, b2 = [int(b0), int(b1), int(b2)]
-tA = (a0,a1,a2)
-tB = (b0,b1,b2)
+b0, b1, b2 = [int(b0), int(b1), int(b2)]
+
+def cast_it(tp):
+    return map(lambda x: int(x),tp)
+
+tA = cast_it([a0,a1,a2])
+tB = cast_it([b0,b1,b2])
+
+_validate = list()
+_validate.extend(tA)
+_validate.extend(tB)
+invalid= filter(lambda x: x>100 or x<1,_validate)
+
+if invalid:
+    raise Exception('ARGHHH damn! wrong input must be between 1 and 100')
+
+del _validate
+del invalid
 result = solve(a0, a1, a2, b0, b1, b2)
+
 print " ".join(map(str, result))
+
+del tA
+del tB
