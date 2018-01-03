d52411a4b1142c4ee3658fd3a21931f552f60eb8
V <v@localhost.localdomain>
2018-01-02 23:51:43 -0200

    setting it up... working on this algorithm

---
+++ challenges/2d-array.py
@@ -0,0 +1,40 @@
+#https://www.hackerrank.com/challenges/2d-array/problem
+import sys,os
+import math
+table=[]
+print 'set the table size'
+N =6
+
+table=[[2 , 4 , 5 , 6 ,727, 453],
+[2 , 4 , 5 , 6 ,77, 43],
+[29 , 448 , 275 , 36 ,717, 6543],
+[25 , 14 , 59 , 61 ,7327, 435],
+[23 , 141 , 95 , 161 ,7217, 643],
+[21 , 42 , 556 , 62 ,757, 423],
+[12 , 445 , 53 , 16 ,717, 143]]
+
+def is_valid_HG(tb,col_index):
+        VALID_COLUMN_LENGTH=3
+        VALID_ROW_LENGTH=3
+        is_valid= True
+        row_length = len(tb) #index that i am
+        #validate columns size
+        is_valid&= row_length>=VALID_ROW_LENGTH
+        if is_valid:
+            column_length = len(tb[col_index:])
+            is_valid&= column_length>=VALID_COLUMN_LENGTH
+        return is_valid
+
+def iterate_table(table):
+    length =  len(table)
+    j=i = 0
+    while i< length:
+        row = table[i]
+        print row
+        while j< len(row):
+            if is_valid_HG(table[i],j):
+                print 'row', row
+
+
+
+iterate_table(table)
--- challenges/diagonal_difference.py
+++ challenges/diagonal_difference.py
@@ -25,15 +25,26 @@ final
 15
 
 """
+"""
+N dimensions
+|1| 1 5 4
+|2| 2 4 5
+|3| 4
+|4| 4 6 8
+|5| 1 1 1 1
 
+"""
 #!/bin/python
 
 import sys
 
-
+print 'Set a given size of an array'
 N = int(raw_input())
 difference = 0
 for i in xrange(N):
+    print 'Set a dimension of an array'
     row = raw_input().split()
     difference += (int(row[i]) - int(row[N-1-i]))
+    print row, 'row'
+    print difference, 'diff'
 print abs(difference)
