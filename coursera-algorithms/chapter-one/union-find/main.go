a8adb9243b65b9dee1c4f7a32fb5d29d9d446ac4
vinicius fagundes <vfagundes@viniciuss-MacBook-Pro.local>
2022-08-27 13:28:23 -0300

    added some algorithms in golang for practicing like stacks, queues, etc..

---
+++ coursera-algorithms/chapter-one/union-find/main.go
@@ -0,0 +1,23 @@
+package main
+
+
+func UF()
+{
+   int N = StdIn.readInt();
+   UF uf = new UF(N);
+   
+   while (!StdIn.isEmpty()){
+      int p = StdIn.readInt();
+      int q = StdIn.readInt();
+      if (!uf.connected(p, q))
+      {
+         uf.union(p, q);
+         StdOut.println(p + " " + q);
+      }
+}
+ }
+
+
+func main() {
+	UF()
+}
