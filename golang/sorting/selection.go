0982964f062af67965fc5ccb58463d7ad0e5ee1f
vinicius fagundes <vfagundes@viniciuss-MacBook-Pro.local>
2022-08-31 18:25:52 -0300

    selection sort implemented

--- golang/sorting/selection.go
+++ golang/sorting/selection.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const TOTAL_SIZE = 10
+
+var data = func() *[]int {
+
+	L := make([]int, TOTAL_SIZE)
+	for i := TOTAL_SIZE - 1; i >= 0; i-- {
+		L[i] = len(L) - i
+	}
+	return &L
+}
 
 func main() {
-	fmt.Println("voila")
+	L := data()
+	selectionSort := func(l *[]int) *[]int {
+		for i := 0; i < len(*l); i++ {
+			for j := i + 1; j < len(*l); j++ {
+				if (*l)[i] > (*l)[j] {
+					(*l)[i] ^= (*l)[j]
+					(*l)[j] ^= (*l)[i]
+					(*l)[i] ^= (*l)[j]
+				}
+			}
+
+		}
+
+		return l
+	}
+
+	fmt.Println(selectionSort(L))
 }
