a53ef33518be2e8133c2d0d73751ab379a50bc71
vinicius <vfagundes>
2024-01-07 15:54:37 +0100

    add paginate handler solution, remove unused references of javascript.

--- challenges/algorithms/paginate_handler.py
+++ challenges/algorithms/paginate_handler.py
@@ -1,25 +1,21 @@
 class PaginateHandler:
     @classmethod
     def distribute_array(cls, array: list, distribution_number: int):
-
-        if distribution_number >= len(array):
-            distribution_number = int(len(array)) + 1
-        i = 0
-        grouped_urls = []
-        while i < len(array):
-            maximum_index = int(len(array) / distribution_number) + i
-            url_group = array[i:maximum_index]
-            grouped_urls.append(url_group)
-            i = maximum_index
+        distribution_number = max(1, min(distribution_number, len(array)))
+        group_size = max(1, len(array) // distribution_number)
+        grouped_urls = [array[i:i + group_size] for i in range(0, len(array), group_size)]
         return grouped_urls
     
 
 def run():
     arrTest1= [x for x in range(31)]
     arrTest2= [x for x in range(1001)]
+    arrTest3= [x for x in range(347)]
     result1 = PaginateHandler.distribute_array(arrTest1,5)
     result2 = PaginateHandler.distribute_array(arrTest2,6)
+    result3 = PaginateHandler.distribute_array(arrTest3,12)
     assert len(result1) == 6
     assert len(result2) == 7
+    assert len(result3) == 13
 
 run()
